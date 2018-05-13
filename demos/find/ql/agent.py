807f696c98d5f75b4abfd76a9cf6254022fcc313
Alexander Schöberl <alexander.schoeberl@gmail.com>
2018-05-13 20:46:53 +0200

    Added batch option

--- demos/find/ql/agent.py
+++ demos/find/ql/agent.py
@@ -3,11 +3,13 @@ import numpy as np
 from zeppelin.utils import Transitions
 from .models import Model
 from zeppelin import Agent as BaseAgent
+from zeppelin.utils import discount
 
 
 class Agent(BaseAgent):
-	def __init__(self, name, dimensions, gamma=0.95, epsilon=1, decay=1-1e-3):
+	def __init__(self, name, dimensions, batch=10, gamma=0.95, epsilon=1, decay=1-1e-3):
 		super().__init__(name)
+		self.batch = batch
 		self.gamma = gamma
 		self.epsilon = epsilon
 		self.decay = decay
@@ -24,8 +26,8 @@ class Agent(BaseAgent):
 	def react(self, position, reward=0, done=False):
 		action = self.respond(position)
 		self.memory.store(position.copy(), action, reward, done, position.copy(), perf=reward)
-		if self.age != 0:
-			self.learn()
+		if self.age % self.batch == (self.batch - 1) or done:
+			self.learn(self.batch)
 		if done:
 			self.epsilon *= self.decay
 			self.episode += 1
@@ -34,18 +36,20 @@ class Agent(BaseAgent):
 	
 	def respond(self, position):
 		if np.random.rand() < self.epsilon:
-			return np.random.randint(0,  *self.model.output_shapes[0])
+			return np.random.randint(0, *self.model.output_shapes[0])
 		else:
 			prediction = self.model.predict([[position]])[0][0]
 			return np.argmax(prediction)
 	
-	def learn(self):
-		position, action, reward, done, outcome = self.memory[-2]
-		past_value_prediction = self.model.predict([[position]])[0][0]
-		future_value_prediction = [0] if done else self.model.predict([[outcome]])[0][0]
-		future_value_prediction = np.max(future_value_prediction)
+	def learn(self, number=1):
+		positions, actions, rewards, dones, outcomes = self.memory[-(number+1):-1]
+		if len(positions) >= 1:
+			past_value_predictions = self.model.predict([positions])[0]
+			future_value_prediction = [0] if dones[-1] else self.model.predict([outcomes[-1:]])[0][0]
+			future_value_prediction = [np.max(future_value_prediction)]
 
-		target = past_value_prediction
-		target[action] = reward + self.gamma * future_value_prediction
-		
-		self.model.fit([[position]], [[target]])
+			targets = past_value_predictions
+			discounted = discount(np.concatenate((rewards, future_value_prediction)), self.gamma)
+			targets[range(len(targets)), actions] = discounted[:-1]
+
+			self.model.fit([positions], [targets])
