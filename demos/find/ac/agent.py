6b279c79046c534f3968a443cc0d2c4f481b439d
Alexander Schöberl <alexander.schoeberl@gmail.com>
2018-05-13 20:04:45 +0200

    Added batch option

--- demos/find/ac/agent.py
+++ demos/find/ac/agent.py
@@ -7,8 +7,9 @@ from zeppelin.utils import discount
 
 
 class Agent(BaseAgent):
-	def __init__(self, name, dimensions, gamma=0.95, epsilon=1, decay=1-1e-3):
+	def __init__(self, name, dimensions, batch=10, gamma=0.95, epsilon=1, decay=1-1e-3):
 		super().__init__(name)
+		self.batch = batch
 		self.gamma = gamma
 		self.epsilon = epsilon
 		self.decay = decay
@@ -19,16 +20,16 @@ class Agent(BaseAgent):
 		self.memory = Transitions(
 			cause_keys=['positions', 'actions'],
 			effect_keys=['rewards', 'dones', 'outcomes'],
-			extra_keys=['perf'],
-			maxlen=10
+			extra_keys=['perf']
 		)
 	
 	def react(self, position, reward=0, done=False):
 		action = self.respond(position)
 		self.memory.store(position.copy(), action, reward, done, position.copy(), perf=reward)
-		if self.age != 0:
-			self.learn()
+		if self.age % self.batch == (self.batch - 1) or done:
+			self.learn(self.batch)
 		if done:
+			self.memory.forget()
 			self.epsilon *= self.decay
 			self.episode += 1
 		self.age += 1
@@ -36,7 +37,7 @@ class Agent(BaseAgent):
 	
 	def respond(self, position):
 		if np.random.rand() < self.epsilon:
-			return np.random.randint(0,  *self.actor_model.output_shapes[0])
+			return np.random.randint(0, *self.actor_model.output_shapes[0])
 		else:
 			prediction = self.actor_model.predict([[position]])[0][0]
 			choice = np.random.choice(prediction, p=prediction)
@@ -44,11 +45,12 @@ class Agent(BaseAgent):
 	
 	def learn(self, number=1):
 		positions, actions, rewards, dones, outcomes = self.memory[-(number+1):-1]
-		past_value_predictions = self.critic_model.predict([positions])[0]
-		future_value_prediction = [0] if dones[-1] else self.critic_model.predict([outcomes[-1:]])[0][0]
+		if len(positions) >= 1:
+			past_value_predictions = self.critic_model.predict([positions])[0]
+			future_value_prediction = [0] if dones[-1] else self.critic_model.predict([outcomes[-1:]])[0][0]
 		
-		targets = discount(np.concatenate((rewards, future_value_prediction)), self.gamma)[:-1].reshape(-1, 1)
-		errors = targets - past_value_predictions
+			targets = discount(np.concatenate((rewards, future_value_prediction)), self.gamma)[:-1].reshape(-1, 1)
+			errors = targets - past_value_predictions
 		
-		self.actor_model.fit([positions], [actions.reshape(-1, 1), errors])
-		self.critic_model.fit([positions], [targets])
+			self.actor_model.fit([positions], [actions.reshape(-1, 1), errors])
+			self.critic_model.fit([positions], [targets])
